ef45113810e7f06bb78856890dd0148f4941bbfa
Zackery Arnold <arnoldz3@udayton.edu>
2017-03-29 22:17:14 -0400

    Updated functions to avoid any recursion. Script #3 specifically benefits by using a deque of return addresses in First-In Last-Out order to navigate the call tree.

--- arnold-lab4-s2.py
+++ arnold-lab4-s2.py
@@ -16,31 +16,45 @@
 ## 
 ## ***********************************************************************************************
 
-## Parse through the database here....
-for seg in Segments():
-    # For each of the defined elements
-    for line in range(seg, SegEnd(seg)):
-        # If it's an instruction
-        if isCode(GetFlags(line)):
-            # Get the mnemonic and increment the count
-            mnem = GetMnem(line)
-            if mnem == 'call' or mnem == 'jmp':
+def FindFunctions(func_list):
+    # This loop will look for any functions called from a given
+    # address. It will walk through the code of a given function
+    # line by line, tracing function calls that branch from the
+    # given control path.
+    n = len(func_list)
+    funcs_found = [False for i in range(n)]
+    checked = []
+ 
+    # We will only ever explore the .text section.
+    for seg in Segments():
+    
+        if(SegName(seg) == ".text"):
+            for addr in range(seg, SegEnd(seg)):
 
-                # Call and JMP take a single argument in the form of an address.
-                # IDA gives a visual text based representation for this argument.
-                # This can be parsed for the correct occurences of library calls.
-                name = GetFunctionName(line)
-                if (name == ""):
-                    name = "[N/A]"
-                op = GetOpnd(line, 0)
-                # print op
-                if (op.find("strcpy") > -1):
-                    print name, ':', hex(line), ":", "strcpy"
-                if (op.find("sprintf") > -1):
-                    print name, ':', hex(line), ":", "sprintf"
-                if (op.find("strncpy") > -1):
-                    print name, ':', hex(line), ":", "strncpy"
-                if (op.find("wcsncpy") > -1):
-                    print name, ':', hex(line), ":", "wcsncpy"
-                if (op.find("swprintf") > -1):
-                    print name, ':', hex(line), ":", "swprintf"
+                if isCode(GetFlags(addr)):
+                    # Get the mnemonic and increment the count
+                    mnem = GetMnem(addr)
+                    if mnem == 'call' or mnem == 'jmp':
+                        name = GetFunctionName(addr)
+                        if (name == ""):
+                            name = "[N/A]"
+                        # Call and JMP take a single argument in the form of an address.
+                        # IDA gives a visual text based representation for this argument.
+                        # This can be parsed for the correct occurences of library calls.
+                        op = GetOpnd(addr, 0)
+                        # Iterate through the function names to check search status.
+                        for i in range(0, n):
+                            if (op.find(func_list[i]) > -1):
+                                print name, ':', hex(addr), ":", func_list[i]
+                                funcs_found[i] = True
+
+    if(not any(funcs_found)):
+        print "None of the functions", func_list, "were called in the program."
+        return 
+    
+def main():
+    func_list = ["strcpy", "sprintf", "strncpy", "wcsncpy", "swprintf"]
+    address = MinEA()
+    FindFunctions(func_list)
+
+main()
--- arnold-lab4-s3.py
+++ arnold-lab4-s3.py
@@ -15,53 +15,78 @@
 ##            due:  March 27, 2017
 ## 
 ## ***********************************************************************************************
+from collections import deque
 
-exp_list = []
-checked = []
+def CollectExports():
+    exp_list = []
+    exp_addr = []
+    for i in range(GetEntryPointQty()):
+        ord = GetEntryOrdinal(i)
+        if ord == 0:
+            continue
+        exp_addr.append(GetEntryPoint(ord))
+        exp_list.append(GetFunctionName(addr))
+    
+    return exp_list, exp_addr
 
-def isBelowExport(addr, exp_list, checked):
-
+def isBelowExport(start, export, func_list):
     # This loop will look for any functions called from a given
     # address. It will walk through the code of a given function
     # line by line, tracing function calls that branch from the
     # given control path.
-    last_ref = addr
+    n = len(func_list)
+    funcs_found = [False for i in range(n)]
+    addr_list = deque([start])
+    checked = []
+ 
+    while( addr_list ):
+        # We have consumed an address, so pop it off the deque.
+        addr = addr_list.popleft()
+        
+        # Collect all refferences from the
+        # currently considered address.
+        refs = CodeRefsFrom(addr, 1)
 
-    for refs in CodeRefsFrom(addr, 1):
-        if refs in checked:
-            continue
-        checked.append(refs)
-        if isCode(GetFlags(refs)):
+        # If there are no refferences to the code and there are no return addresses
+        # then we will exit.
+        if((refs == [] and len(addr_list) == 0) or (all(funcs_found))):
+            if(not any(funcs_found)):
+                print "None of the functions", func_list, "were called from within", export
+            return
+        
+        # If only one reference returns, then it is the next line of code.
+        # If more than one reference returns, then it means the line is a 
+        # jump or a function call. The called function and the return address
+        # are saved so that they can be iterated through.
+        for i in range(0, len(refs)):
+            # Only put up the address if we haven't been there before.
+            if(not refs[i] in checked):
+                addr_list.appendleft(refs[i])
+                checked.append(refs[i])
+
+        # Now we simply must look at the operands of the currently viewed
+        # address and see if it contains the string we are looking for.
+        if isCode(GetFlags(addr)):
             # Get the mnemonic and increment the count
-            mnem = GetMnem(refs)
+            mnem = GetMnem(addr)
             if mnem == 'call' or mnem == 'jmp':
-
+                #print GetDisasm(addr)
                 # Call and JMP take a single argument in the form of an address.
                 # IDA gives a visual text based representation for this argument.
                 # This can be parsed for the correct occurences of library calls.
-                name = GetFunctionName(refs)
-                if (name == ""):
-                    name = "[N/A]"
-                op = GetOpnd(refs, 0)
-                if (op.find("strcpy") > -1):
-                    print exp_list, ':', hex(refs), ":", "strcpy"
-                if (op.find("sprintf") > -1):
-                    print exp_list, ':', hex(refs), ":", "sprintf"
-                if (op.find("strncpy") > -1):
-                    print exp_list, ':', hex(refs), ":", "strncpy"
-                if (op.find("wcsncpy") > -1):
-                    print exp_list, ':', hex(refs), ":", "wcsncpy"
-                if (op.find("swprintf") > -1):
-                    print exp_list, ':', hex(refs), ":", "swprintf"
+                op = GetOpnd(addr, 0)
+                # Iterate through the function names to check search status.
+                for i in range(0, n):
+                    if (op.find(func_list[i]) > -1  and not funcs_found[i]):
+                        print export, ':', hex(addr), ":", func_list[i]
+                        funcs_found[i] = True
 
-        # If this refference is linear, don't bother with recursion.
-        isBelowExport(refs, exp_list, checked)
+    print "Loop Exited.", export
 
-## Must start from the export list. They can be found as follows:
-for i in range(GetEntryPointQty()):
-    ord = GetEntryOrdinal(i)
-    if ord == 0:
-        continue
-    addr = GetEntryPoint(ord)
-    exp_list.append(GetFunctionName(addr))
-    isBelowExport(addr, exp_list[i], checked)
+def main():
+    exports, addresses = CollectExports()
+    func_list = ["strcpy", "sprintf", "strncpy", "wcsncpy", "swprintf"]
+    for i in range(0, len(exports)):
+        isBelowExport(addresses[i], exports[i],func_list)
+
+main()
